2e2cbd9ffa12f2b8de59355c459d0e2db01bb0b9
xuanzhaopeng <xuanzhaopeng@gmail.com>
2017-03-17 16:12:38 +0100

    Support to link tags to testrail

--- src/main/java/net/masterthought/cucumber/json/Tag.java
+++ src/main/java/net/masterthought/cucumber/json/Tag.java
@@ -2,8 +2,12 @@ package net.masterthought.cucumber.json;
 
 import net.masterthought.cucumber.util.Util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Tag {
 
+    private static final String testRailCaseRoot = "https://hotelstonight.testrail.com/index.php?/cases/view/";
     // Start: attributes from JSON file report
     private final String name = null;
     // End: attributes from JSON file report
@@ -19,6 +23,24 @@ public class Tag {
     public static String generateFileName(String tagName) {
         // TODO: the file name should be unique
         return String.format("report-tag_%s.html", Util.toValidFileName(tagName.replace("@", "")).trim());
+    }
+
+    public boolean isTestRailTag() {
+        Pattern pattern = Pattern.compile("^@C\\d+$");
+        Matcher matcher = pattern.matcher(getName());
+        return matcher.matches();
+    }
+
+    public String getTestRailUrl() {
+        return testRailCaseRoot + getTestRailCaseId();
+    }
+
+    private String getTestRailCaseId() {
+        if(getName().length() > 2) {
+            return getName().substring(2);
+        } else {
+            return "";
+        }
     }
 
     @Override
